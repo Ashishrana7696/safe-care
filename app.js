85a67b0dd8f196f1b0dffd0ce8f8f63f570cc279
akash tyagi <tyagiakash874@gmail.com>
2022-07-22 16:49:28 +0530

    login signup apis

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+var ResponseMicros = require('./Macros/ResponseMicros');
 const mongoose = require("mongoose");
 
 //for use env varables
@@ -14,7 +14,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
-
+ResponseMicros.response(app);
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
