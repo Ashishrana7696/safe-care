e07631ae9b55a0f67212d8c5e42d87e9de7a7b47
ashish <ashish171154@gmail.com>
2022-08-18 14:05:20 +0530

    pdf

--- controllers/employeesDetails.js
+++ controllers/employeesDetails.js
@@ -2,6 +2,13 @@
 const employees = require('../Model/employees');
 const salary = require('../Model/salary');
 const helper = require('../helper')
+const fs = require('fs');
+const pdf = require('pdf-creator-node');
+const path = require('path');
+const options = require('../helpers/options');
+const { debugPort } = require('process');
+const {dbo}=require('../app.js');
+// const data = require('../helpers/data');
 async function addEmployees(req, res) {
     try {
 
@@ -54,6 +61,87 @@ async function getExcelFile(req,res) {
     req.employeeDetails = employeeDetails;
     helper.arrayToExcel(req,res); 
 }
+const generatePdf = async (req, res, next) => {
+
+    // console.log("dfdf==========================",dbo);
+    var id1 = req.body.employee_id;
+    const env                     = process.env;
+    const html = fs.readFileSync(path.join(__dirname, '../views/salary.html'), 'utf-8');
+    const filename = Math.random() + '_doc' + '.pdf';
+    let array = [];
+
+    // employees.find().populate('salary_deatils').exec(function(err, employees) {
+    //     if (err) throw err;
+    
+    //     var adTimes = [];
+    //     employees.forEach(function(employee) {
+    //         employee.salary_deatils.forEach(function(salary_deatil) {
+    //             adTimes.push(salary_deatil.adTime);
+    //         });
+    //     });
+    
+    //     response.send(adTimes); // adTimes should contain all addTimes from his friends
+    // });
+
+
+
+
+    const url = env.DB_CLOUD;
+    var MongoClient = require('mongodb').MongoClient;
+    // var url = "mongodb://127.0.0.1:27017/";
+
+     MongoClient.connect(url, function(err, db) {
+             if (err) throw err;
+    var dbo = db.db("welcomedb");
+
+            var dbo = db.db("test");
+            // var query = { employee_id:id1 };
+            dbo.collection('employees').aggregate([              
+            { $lookup:
+                {
+                from: 'salary_details',
+                localField: 'employee_id',
+                foreignField: '_id',
+                as: 'salarydetails'
+                }
+            } ,{
+                '$match': {
+                    'salarydetails.employee_id': id1
+                }
+              }
+            ]).toArray(function(err, res) {
+            if (err) throw err;
+            console.log(JSON.stringify(res));
+            db.close();
+            });
+        });
+ 
+
+    const obj = {
+        prodlist: array,
+        // subtotal: subtotal,
+        // tax: tax,
+        // gtotal: grandtotal
+    }
+    const document = {
+        html: html,
+        data: {
+            products: obj
+        },
+        path: './docs/' + filename
+    }
+    pdf.create(document, options)
+        .then(res => {
+            console.log(res);
+        }).catch(error => {
+            console.log(error);
+        });
+        const filepath = 'http://localhost:3000/docs/' + filename;
+
+        res.render('download', {
+            path: filepath
+        });
+}
 
 async function addSalary(req,res){
  try{
@@ -77,8 +165,10 @@ async function addSalary(req,res){
 
 module.exports = {
     getExcelFile:getExcelFile,
+    // getPdfFile:getPdfFile,
     addEmployees: addEmployees,
     getEmployeeList: getEmployeeList,
     updateEmployeeDetails:updateEmployeeDetails,
-    addSalary:addSalary
+    addSalary:addSalary,
+    generatePdf
 }
